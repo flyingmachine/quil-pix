eb370ece72d4179a160005f2e310e71385ed430e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-07-22 13:18:14 -0400

    don't modify first column

--- src/quil_pix/scan.clj
+++ src/quil_pix/scan.clj
@@ -38,7 +38,9 @@
 
 (defn reduce-pixels [width pixels]
   (reduce (fn [x y]
-            (conj x (avg-colors [y (get x (- (count x) width)) (last x)])))
+            (if (= 1 (mod (count x) width))
+              (conj x y)
+              (conj x (avg-colors [y (get x (- (count x) width)) (last x)]))))
           (subvec pixels 0 (inc width))
           (subvec pixels (inc width))))
 
